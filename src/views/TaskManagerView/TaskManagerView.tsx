e837f76318c0f3bf6373ab91eda4f1274ca5866d
Dimitris Zerdevas <42102327+dimzerdevas@users.noreply.github.com>
2024-12-06 22:32:31 +0200

    add persistent drag and drop functionality, change favicon of page

--- src/views/TaskManagerView/TaskManagerView.tsx
+++ src/views/TaskManagerView/TaskManagerView.tsx
@@ -8,6 +8,8 @@ import { useChuckNorrisJoke } from "./queries";
 import { Loader } from "../../components";
 import { useSnackbar } from "notistack";
 import { useEffect } from "react";
+import { DragDropContext, Draggable, DropResult } from "react-beautiful-dnd";
+import { StrictModeDroppable } from "../../components/StrictModeDroppable";
 
 export const TaskManagerView = () => {
   const {
@@ -21,6 +23,7 @@ export const TaskManagerView = () => {
     toggleComplete,
     filter,
     setFilter,
+    setTasks,
   } = useTaskManager();
 
   const {
@@ -47,6 +50,29 @@ export const TaskManagerView = () => {
     }
   }, [chuckNorrisJoke, enqueueSnackbar, isErrorChuckNorrisJoke]);
 
+  const handleDragDrop = (results: DropResult) => {
+    const { source, destination, type } = results;
+
+    if (!destination) return;
+    if (
+      source.droppableId === destination.droppableId &&
+      source.index === destination.index
+    )
+      return;
+
+    if (type === "group") {
+      const reOrderedTasks = structuredClone(tasks);
+      const sourceIndex = source.index;
+      const destinationIndex = destination.index;
+
+      const [removedTask] = reOrderedTasks.splice(sourceIndex, 1);
+
+      reOrderedTasks.splice(destinationIndex, 0, removedTask);
+
+      setTasks(reOrderedTasks);
+    }
+  };
+
   return (
     <Container>
       {isLoadingChuckNorrisJoke && <Loader />}
@@ -65,21 +91,43 @@ export const TaskManagerView = () => {
         </IconButton>
       </AddTaskContainer>
       <Button onClick={() => refetchChuckNorrisJoke()}>Bored?</Button>
-      <List>
-        {tasks.map(({ isEditing, text, id, completed }) => (
-          <TaskItem
-            editTask={editTask}
-            toggleEdit={toggleEdit}
-            deleteTask={deleteTask}
-            toggleComplete={toggleComplete}
-            isEditing={isEditing}
-            text={text}
-            completed={completed}
-            key={id}
-            id={id}
-          />
-        ))}
-      </List>
+      <DragDropContext onDragEnd={handleDragDrop}>
+        <List>
+          <StrictModeDroppable droppableId="ROOT" type="group">
+            {(provided) => (
+              <div {...provided.droppableProps} ref={provided.innerRef}>
+                {tasks.map(({ isEditing, text, id, completed }, index) => (
+                  <Draggable
+                    draggableId={String(id + index)}
+                    key={id}
+                    index={index}
+                  >
+                    {(provided) => (
+                      <div
+                        {...provided.dragHandleProps}
+                        {...provided.draggableProps}
+                        ref={provided.innerRef}
+                      >
+                        <TaskItem
+                          editTask={editTask}
+                          toggleEdit={toggleEdit}
+                          deleteTask={deleteTask}
+                          toggleComplete={toggleComplete}
+                          isEditing={isEditing}
+                          text={text}
+                          completed={completed}
+                          key={id}
+                          id={id}
+                        />
+                      </div>
+                    )}
+                  </Draggable>
+                ))}
+              </div>
+            )}
+          </StrictModeDroppable>
+        </List>
+      </DragDropContext>
     </Container>
   );
 };
